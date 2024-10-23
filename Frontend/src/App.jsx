d390d95d4c43884202e640cf167f561c05aae53d
Parth Dwivedi <99666524+Parth4git@users.noreply.github.com>
2024-10-23 23:23:07 +0530

    frontend second change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={appRouter} />
+    </>
+  );
 }
 
 export default App;
