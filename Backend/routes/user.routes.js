6da4b9875f8e5a647a9db59cc655b3faff2d22bf
Alphasf9 <134485025+Alphasf9@users.noreply.github.com>
2024-11-03 11:49:29 +0530

    Error can be caused on this push

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import { login, logout, register, updateProfile } from '../controllers/user.controller.js';
 import isAuthenticated from '../middleware/isAuthenticated.js';
+import { singleUpload } from '../middleware/multer.js';
 
 
 const router = express.Router();
 
 
-router.route('/register').post(register);
+router.route('/register').post(singleUpload, register);
 router.route('/login').post(login);
 router.route('/logout').post(isAuthenticated, logout);
 router.route('/updateProfile').post(isAuthenticated, updateProfile);
 
 export default router;
+
