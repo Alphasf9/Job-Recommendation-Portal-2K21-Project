b92e43a16c2ed1344cfdd2a7ea160ee7e65cbc09
MOHD HASEEB ALI <134485025+Alphasf9@users.noreply.github.com>
2024-11-03 11:54:03 +0530

    Update multer.js

--- Backend/middleware/multer.js
+++ Backend/middleware/multer.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 import multer from 'multer'
 
 const storage = multer.memoryStorage();
 
 export const singleUpload = multer({ storage }).single('file');
-
-=======
-import multer from 'multer'
-
-const storage = multer.memoryStorage();
-
-export const singleUpload = multer({ storage }).single('file');
-
->>>>>>> ca04c13ac29814a6e1da2ade11b554cb71d21910
